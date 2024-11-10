fc07c1f40940bca1a15ef3a33c05ade6526e8d96
Kshawayushi28 <shawayushi110@gmail.com>
2024-11-11 00:51:24 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+//let score="33abc"
+//let score=null
+//let score=undefined
+//let score=true
+let score="Ayushi"
+
+//const{score}=req.body
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+//let isLoggedIn=true
+//let isLoggedIn=1
+//let isLoggedIn=""
+let isLoggedIn="Ayushi"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true ; 0=>false
+//""=>false
+//"Ayushi"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
