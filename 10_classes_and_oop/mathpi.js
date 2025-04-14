058ef4fad24e5b31e93c9ae7e4bcf76b4b43c353
Ayushi Kumari Shaw <144890008+Kshawayushi28@users.noreply.github.com>
2025-04-14 16:45:36 +0000

    mathpi

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,6 +1,6 @@
 const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
 
-console.log(descripter);
+// console.log(descripter);
 
 // console.log(Math.PI);
 // Math.PI = 5
@@ -9,18 +9,25 @@ console.log(descripter);
 const chai = {
     name : 'ginger chai',
     price : 250,
-    isAvailable : true
+    isAvailable : true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
 }
 
 console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 
-// Object.defineProperty(chai,'name',{
-//     writable : false,
-//     enumerable : false
-// }) 
+Object.defineProperty(chai,'name',{
+    // writable : false,
+    enumerable : true,
+}) 
 
-// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 
-for (let [key,value] of chai) {
-    console.log(`${key}`)
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}:${value}`);
+    }
+    
 }
